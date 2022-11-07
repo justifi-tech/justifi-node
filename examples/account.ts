99c15285f3f869f03677558faa32129e028474d0
Caio Rosa <>
2022-11-07 19:33:51 -0300

    feat: improve auth token example and add account example

---
+++ examples/account.ts
@@ -0,0 +1,41 @@
+import { AccountStatus } from "../lib/account";
+import { Justifi } from "../lib/client";
+import { getCredentials } from "./auth_token";
+
+const client = Justifi.client().withCredentials(getCredentials());
+
+if (process.argv.length < 3) {
+  console.log("Request name argument is required");
+  process.exit(1);
+}
+
+switch (process.argv[2]) {
+  case "createSellerAccount":
+    client
+      .createSellerAccount("justifi node sdk")
+      .then(console.log)
+      .catch(console.log);
+    break;
+
+  case "listSellerAccountsNoFilter":
+    console.log("hello");
+    client.listSellerAccounts().then(console.log).catch(console.log);
+    break;
+
+  case "listSellerAccountsWithFilter":
+    client
+      .listSellerAccounts(AccountStatus.Archived)
+      .then(console.log)
+      .catch(console.log);
+    break;
+
+  case "getSellerAccount":
+    client
+      .getSellerAccount("<seller_account_id>")
+      .then(console.log)
+      .catch(console.log);
+    break;
+
+  default:
+    break;
+}
